ef6abc6081b2db6a962d3d30c7c25b8f2094e529
Cabin <sdn.warehouse@gmail.com>
2024-12-02 17:21:16 +0700

    metadata

--- next.config.js
+++ next.config.js
@@ -1,41 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'blog.sdnthailand.com',
-        port: '',
-        pathname: '/images/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'blog.sdnthailand.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'sdnthailand.com',
-        port: '',
-        pathname: '/images/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'secure.gravatar.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: '*.gravatar.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: '*.wp.com',
-        port: '',
-        pathname: '/**',
-      }
+    domains: [
+      'blog.sdnthailand.com',
+      'sdnthailand.com',
+      'secure.gravatar.com',
+      '0.gravatar.com',
+      '1.gravatar.com',
+      '2.gravatar.com',
+      '3.gravatar.com',
+      'i0.wp.com',
+      'i1.wp.com',
+      'i2.wp.com'
     ],
   },
 };
