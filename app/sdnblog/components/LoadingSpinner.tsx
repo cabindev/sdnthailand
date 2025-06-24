d5a2528eb185ac1702c82aa2c47379476e733b34
cabindev <sdn.warehouse@gmail.com>
2025-06-24 18:16:21 +0700

    update color

--- app/sdnblog/components/LoadingSpinner.tsx
+++ app/sdnblog/components/LoadingSpinner.tsx
@@ -3,7 +3,7 @@ export default function LoadingSpinner() {
     return (
       <div className="container mx-auto px-4 py-20">
         <div className="flex justify-center items-center min-h-[60vh]">
-          <span className="loading loading-dots loading-lg text-orange-500"></span>
+          <span className="loading loading-dots loading-lg text-orange-600"></span>
         </div>
       </div>
     )
